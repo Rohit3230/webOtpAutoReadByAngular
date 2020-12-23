b851968617227ae40bc069f13fdd2fbe71b00cd8
rohit.kumar <rohit.kumar@marwadigroup.in>
2020-12-23 18:07:20 +0530

    commit code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webOtpAutoReadByAngular';
+  mainObj:any={};
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  myOTP:any;
+  ngAfterViewInit() {
+    if ('OTPCredential' in window) {
+        this.mainObj.isWebOtpSupported = true;
+        debugger;
+        window.addEventListener('DOMContentLoaded', e => {
+            debugger;
+        const input = document.querySelector('input[autocomplete="one-time-code"]');
+        if (!input) return;
+        const ac = new AbortController();
+        const form = input.closest('form');
+        debugger;
+        if (form) {
+            debugger;
+            form.addEventListener('submit', e => {
+            ac.abort();
+            });
+        }
+        var reqObj =  {
+          otp: { transport:['sms'] },
+          signal: ac.signal
+      };
+        navigator.credentials.get(
+          reqObj
+        ).then((otp:any) => {
+            debugger;
+            if(
+              otp
+            ){
+              if(
+                otp && otp.code
+              ){
+                // alert('GOT OTP***'+ otp.code);
+                // input.value = otp.code;
+                this.myOTP = otp.code;
+              }
+            }
+            
+            // if (form) form.submit();
+        }).catch(err => {
+            debugger;
+            console.log(err);
+        });
+        });
+    }else{
+      // this.myOTP = 521456;
+      this.mainObj.isWebOtpSupported = false;
+      // alert('Web OTP API not supported, Please enter manually.');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule  } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
